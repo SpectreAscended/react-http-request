9f51dc106ab37e6ec8c24e0a2fa2262f4cf62e13
Cory Pollard <astandard87@gmail.com>
2023-01-23 13:30:00 -0600

    created http request to swapi api

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    try {
+      const res = await fetch('https://swapi.dev/api/films');
+
+      if (!res.ok) throw new Error('Problem fetching data');
+
+      const data = await res.json();
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  console.log(movies);
 
   return (
     <>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </>
   );
